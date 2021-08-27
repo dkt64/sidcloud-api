d6ec1a6a803bee8624695e62f3a18cbff88ce12f
DKT <b.apanasewicz@gmail.com>
2021-08-27 23:52:50 +0200

    host1 update

--- update-host1.bat
+++ update-host1.bat
@@ -1,4 +1,4 @@
-ssh -i C:\Users\admin\.ssh\host1 root@185.157.81.123 /bin/bash -c 'rm -R /root/go/src/github.com/dkt64/sidcloud-api/dist'
-c:\tools\pscp -P 22 -i %USERPROFILE%\.ssh\host1.ppk -r -v dist root@185.157.81.123:/root/go/src/github.com/dkt64/sidcloud-api
-c:\tools\pscp -P 22 -i %USERPROFILE%\.ssh\host1.ppk sidcloud.go root@185.157.81.123:/root/go/src/github.com/dkt64/sidcloud-api
-ssh -i C:\Users\admin\.ssh\host1 root@185.157.81.123 /bin/bash -c 'systemctl restart sidcloud'
+ssh -i "C:\Users\Bartosz Apanasewicz\.ssh\host1" root@185.157.81.123 /bin/bash -c 'rm -R /root/go/src/github.com/dkt64/sidcloud-api/dist'
+c:\tools\pscp -P 22 -i "%USERPROFILE%\.ssh\host1.ppk" -r -v dist root@185.157.81.123:/root/go/src/github.com/dkt64/sidcloud-api
+c:\tools\pscp -P 22 -i "%USERPROFILE%\.ssh\host1.ppk" sidcloud.go root@185.157.81.123:/root/go/src/github.com/dkt64/sidcloud-api
+ssh -i "C:\Users\Bartosz Apanasewicz\.ssh\host1" root@185.157.81.123 /bin/bash -c 'systemctl restart sidcloud'
